aa6252058268d48bb52188b416e8af4021b5c38c
tempapy <60364152+tempapy@users.noreply.github.com>
2022-04-14 22:34:14 +0200

    better links

--- en/simple_script.js
+++ en/simple_script.js
@@ -4,4 +4,6 @@ var elements = document.getElementsByTagName("a")
 for(let i = 0; i < elements.length; i++){
 	var link = elements[i].getAttribute("href").slice(12);
 	elements[i].setAttribute("href", "https://tempapy.github.io/SuperMonier/?searchslp1="+link);
+	elements[i].setAttribute("target", "_blank");
+	//<a href="javascript:window.open('print.html', 'newwindow', 'width=300,height=250')">Print</a>
 }
