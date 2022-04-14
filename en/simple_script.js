7f91ec7c32d7509966d68d2332918e60fbd5a8fb
tempapy <60364152+tempapy@users.noreply.github.com>
2022-04-15 00:00:37 +0200

    Update simple_script.js

--- en/simple_script.js
+++ en/simple_script.js
@@ -5,8 +5,8 @@ var elements = document.getElementsByTagName("a")
 for(let i = 0; i < elements.length; i++){
 	var link = elements[i].getAttribute("href").slice(12);
 	//elements[i].setAttribute("href", "../../html_entries/"+link+".html");
-	elements[i].setAttribute("href", "https://tempapy.github.io/SuperMonier/?searchslp1="+link);
-	//elements[i].setAttribute("onclick", "window.open('https://tempapy.github.io/SuperMonier/?searchslp1="+link+"', '_system', 'width=500,height=600'); return false;");
+	//elements[i].setAttribute("href", "https://tempapy.github.io/SuperMonier/?searchslp1="+link);
+	elements[i].setAttribute("onclick", "window.open('https://tempapy.github.io/SuperMonier/?searchslp1="+link+"', 'newwindow', 'width=500,height=600'); return false;");
 
 	elements[i].setAttribute("target", "_blank");
 	//<a href=>Print</a>
