3625886d290c37f7908f1e2d23f6d89968b7587e
tempapy <60364152+tempapy@users.noreply.github.com>
2022-04-17 22:30:22 +0200

    stc

---
+++ stc/simple_script.js
@@ -0,0 +1,15 @@
+
+
+
+
+var elements = document.getElementsByTagName("a")
+				
+for(let i = 0; i < elements.length; i++){
+	var link = elements[i].getAttribute("href").slice(15);
+	//elements[i].setAttribute("href", "../../html_entries/"+link+".html");
+	elements[i].setAttribute("href", "../../?searchslp1="+link);
+	//elements[i].setAttribute("onclick", "window.open('https://tempapy.github.io/SuperMonier/?searchslp1="+link+"', 'newwindow', 'width=500,height=600'); return false;");
+
+	//elements[i].setAttribute("target", "_blank");
+	//<a href=>Print</a>
+}
