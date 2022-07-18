084b2455b976a07506d3c31015b61055d4c3984f
tempapy <60364152+tempapy@users.noreply.github.com>
2022-07-18 14:25:12 +0200

    fixed ancestor.html

--- bor/simple_script.js
+++
@@ -1,4 +0,0 @@
-
-
-var elements = document.getElementsByClassName("pc")
-for (let i = 0; i < elements.length; i++) elements[i].href="https://www.sanskrit-lexicon.uni-koeln.de/scans/csl-apidev/servepdf.php?dict=BOR&page="+elements[i].getAttribute("href").slice(21);
